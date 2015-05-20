d0d0c8dd9ce6761264624dd65120cb9d58e66df4
Ashleigh <ashleighalmeida@gmail.com>
2015-05-20 16:16:32 -0400

    Completed basic sign in and  registration with styling

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,7 +1,7 @@
 class UserSessionsController < ApplicationController
 
   def create
-  user = login(params[:email], params[:password], params[:remember_me])
+  user = login(params[:email], params[:password], params[:remember_me], params[:handle])
   if user
     redirect_back_or_to root_url, :notice => "Logged in!"
   else
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   resources :password_resets, only: [:edit, :create, :update, :new]
   resources :user_sessions, only: [:new, :create, :destroy]
   resources :users
-get "signup" => "users#new", :as => "signup"
-get 'login' => 'user_sessions#new', :as => :login
-post 'logout' => 'user_sessions#destroy', :as => :logout
+  get "signup" => "users#new", :as => "signup"
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
+  root 'user_sessions#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
