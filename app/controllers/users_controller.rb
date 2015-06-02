7cbe1249f2a2ade628748384b02084893a594840
Ashleigh <ashleighalmeida@gmail.com>
2015-06-02 11:30:56 -0400

    user profile edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    user = edit(params[:email], params[:gender], params[:location], params[:bio], params[:name])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
