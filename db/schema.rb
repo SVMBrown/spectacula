ad4ab6b5ca90fe7d2a95ee10a49023d535b8ee3b
Cody <codyjjwbrown@gmail.com>
2015-06-03 15:28:43 -0400

    bourbon installed

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150530221652) do
+ActiveRecord::Schema.define(version: 20150602150018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "comment"
+    t.text     "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "game_players", force: :cascade do |t|
     t.integer  "player_id"
@@ -31,20 +38,6 @@ ActiveRecord::Schema.define(version: 20150530221652) do
     t.integer  "capacity"
   end
 
-  create_table "profiles", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "name"
-    t.date     "birthday"
-    t.string   "sex"
-    t.text     "introduction"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
-  end
-
-  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
-
   create_table "users", force: :cascade do |t|
     t.string   "handle"
     t.string   "email",                           null: false
@@ -57,15 +50,10 @@ ActiveRecord::Schema.define(version: 20150530221652) do
     t.string   "reset_password_token"
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", using: :btree
 
-  add_foreign_key "profiles", "users"
 end
