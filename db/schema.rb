cd4f260520f79eaa1a188dc06059866975997703
Ashleigh <ashleighalmeida@gmail.com>
2015-06-03 15:41:13 -0400

    comments migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150530221652) do
+ActiveRecord::Schema.define(version: 20150602150018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "comment"
+    t.text     "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "game_players", force: :cascade do |t|
     t.integer  "player_id"
