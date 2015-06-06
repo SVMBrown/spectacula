b940b03e599de269c535f8efb3d69d4b748ee9d0
SVMBrown <scotb38@gmail.com>
2015-06-06 00:33:26 -0400

    trying secure websockets on heroku

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -15,7 +15,7 @@ var setupWS = function (ws) {
 var ready = function () {
   var body = document.getElementsByTagName('body')[0];
   if(body.classList.contains('games') && body.classList.contains('show')) {
-    var ws = new WebSocket("ws://" + window.location.host + "/games/" + $('#container').data('game-id') + "/play");
+    var ws = new WebSocket("wss://" + window.location.host + "/games/" + $('#container').data('game-id') + "/play");
     setupWS(ws);
     React.render(React.createElement(GameClient, {websocket: ws}), $('div#container')[0]);
   }
