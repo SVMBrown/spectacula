b507023de6e4ad2471b7f3774f7d4ebb046700bf
Cody <codyjjwbrown@gmail.com>
2015-06-09 11:06:40 -0400

    fixed leaderboard word wrap

--- db/schema.rb
+++ db/schema.rb
@@ -24,63 +24,6 @@ ActiveRecord::Schema.define(version: 20150608215427) do
     t.integer  "subject_id"
   end
 
-  create_table "commontator_comments", force: :cascade do |t|
-    t.string   "creator_type"
-    t.integer  "creator_id"
-    t.string   "editor_type"
-    t.integer  "editor_id"
-    t.integer  "thread_id",                     null: false
-    t.text     "body",                          null: false
-    t.datetime "deleted_at"
-    t.integer  "cached_votes_up",   default: 0
-    t.integer  "cached_votes_down", default: 0
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "commontator_comments", ["cached_votes_down"], name: "index_commontator_comments_on_cached_votes_down", using: :btree
-  add_index "commontator_comments", ["cached_votes_up"], name: "index_commontator_comments_on_cached_votes_up", using: :btree
-  add_index "commontator_comments", ["creator_id", "creator_type", "thread_id"], name: "index_commontator_comments_on_c_id_and_c_type_and_t_id", using: :btree
-  add_index "commontator_comments", ["thread_id", "created_at"], name: "index_commontator_comments_on_thread_id_and_created_at", using: :btree
-
-  create_table "commontator_subscriptions", force: :cascade do |t|
-    t.string   "subscriber_type", null: false
-    t.integer  "subscriber_id",   null: false
-    t.integer  "thread_id",       null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "commontator_subscriptions", ["subscriber_id", "subscriber_type", "thread_id"], name: "index_commontator_subscriptions_on_s_id_and_s_type_and_t_id", unique: true, using: :btree
-  add_index "commontator_subscriptions", ["thread_id"], name: "index_commontator_subscriptions_on_thread_id", using: :btree
-
-  create_table "commontator_threads", force: :cascade do |t|
-    t.string   "commontable_type"
-    t.integer  "commontable_id"
-    t.datetime "closed_at"
-    t.string   "closer_type"
-    t.integer  "closer_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "commontator_threads", ["commontable_id", "commontable_type"], name: "index_commontator_threads_on_c_id_and_c_type", unique: true, using: :btree
-
-  create_table "conversations", force: :cascade do |t|
-    t.integer  "sender_id"
-    t.integer  "recipient_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "conversations", ["recipient_id"], name: "index_conversations_on_recipient_id", using: :btree
-  add_index "conversations", ["sender_id"], name: "index_conversations_on_sender_id", using: :btree
-
-  create_table "friendships", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "game_players", force: :cascade do |t|
     t.integer  "player_id"
     t.integer  "game_id"
@@ -96,38 +39,6 @@ ActiveRecord::Schema.define(version: 20150608215427) do
     t.datetime "updated_at", null: false
     t.integer  "capacity"
     t.text     "state"
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.text     "body"
-    t.integer  "conversation_id"
-    t.integer  "user_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
-  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
-
-  create_table "profiles", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "name"
-    t.date     "birthday"
-    t.string   "sex"
-    t.text     "introduction"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
-  end
-
-  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
-
-  create_table "reviews", force: :cascade do |t|
-    t.text     "comment"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -152,5 +63,4 @@ ActiveRecord::Schema.define(version: 20150608215427) do
   add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", using: :btree
 
-  add_foreign_key "messages", "conversations"
 end
