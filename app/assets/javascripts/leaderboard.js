d1559ff178f5adfbb22f9a07a6391f23362b987c
Cody <codyjjwbrown@gmail.com>
2015-06-09 11:51:36 -0400

    change landing image

--- app/assets/javascripts/leaderboard.js
+++ app/assets/javascripts/leaderboard.js
@@ -1,6 +1,6 @@
 $(function() {
   if($('body')[0].classList.contains('leaderboard')){
-    $('.clearfix').css({display: inlineBlock;});
+    $('.clearfix').css({display: inlineBlock});
     users.forEach(function(element) {
       console.log("wins" + element.wins);
     });
