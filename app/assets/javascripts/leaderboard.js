5c35265189b284e39d9f334d6df4f1e89e4f3767
Ashleigh <ashleighalmeida@gmail.com>
2015-06-08 23:08:50 -0400

    leaderboard table fix

--- app/assets/javascripts/leaderboard.js
+++ app/assets/javascripts/leaderboard.js
@@ -1,5 +1,6 @@
 $(function() {
   if($('body')[0].classList.contains('leaderboard')){
+    $('.clearfix').css({display: inlineBlock;});
     users.forEach(function(element) {
       console.log("wins" + element.wins);
     });
--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,6 +1,6 @@
 class LeaderboardController < ApplicationController
   def index
-    @users=User.all
+    @users=User.all.sort_by{|user| user.win_rate}.reverse!
    
   end
 
